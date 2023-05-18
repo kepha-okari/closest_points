5e7855a074ca7ed9a83ac4ef4ee9e01a68b88ad2
kepha-okari <kephaokari@gmail.com>
2023-05-18 05:14:10 +0300

    add model Point

--- closest_points/api/models.py
+++ closest_points/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Point(models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    closest_point_x = models.IntegerField()
+    closest_point_y = models.IntegerField()
